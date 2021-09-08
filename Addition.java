762d9ea9e5f8fd87176fcb8802365d10528b08ec
unknown <palseyesh14@gmail.com>
2021-09-08 15:05:30 +0530

    added addition logic

--- Addition.java
+++ Addition.java
@@ -2,6 +2,17 @@ public class Addition
 {
 public static void main(String args[])
 {
-System.out.println("Welcome");
+	int a =5670, b=3784;
+
+	Addition ad = new Addition();
+	int result = ad.sum(a,b);
+	System.out.println(result);
+	
+	
 }
+	int sum(int a, int b)
+	{
+		int c = a+b;
+		return c;
+	}
 }
